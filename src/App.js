623e37f44edb91ff13345319393fa6fa91f3d0bc
Mirgude Mahesh <mirgudemahesh2002@gmail.com>
2025-09-30 16:31:38 +0530

    extra

--- src/App.js
+++ src/App.js
@@ -33,27 +33,10 @@ function App() {
  <Route path="/selection" element={<Selection />} />
 
 
-            {/* protected landing route */}
-            {/* <Route
-              path="/"
-              element={role ? <Selection/> : <Loginpage />}
-            /> */}
+         
 
-            {/* <Route exact path='/info' element={<MyChats />} />
-            <Route exact path='/Performance' element={<Home />} />
-            <Route exact path='/TeamBuild' element={<Performance />} />
-            <Route exact path='/Hygine' element={<Miscfiles />} />
-            <Route exact path='/Compliance' element={<Commitment />} />
-            <Route exact path='/Selection' element={<Selection />} />
-            <Route exact path='/MyChats' element={<MyChats />} />
-          */}
-            {/* dashboard routes */}
-            {/* <Route exact path='/profile/:name/Review' element={<UserFinalReport />} />
-            <Route exact path='/profile/:name/Performance' element={<UserHome />} />
-            <Route exact path='/profile/:name/TeamBuild' element={<UserPerformance />} />
-            <Route exact path='/profile/:name/Hygine' element={<UserMiscfiles />} />
-            <Route exact path='/profile/:name/Compliance' element={<UserCommitment />} />
-            <Route exact path='/profile/:name/Chats' element={<Chats />} /> */}
+           
+          
           </Routes>
         </Layout>
       </Router>
